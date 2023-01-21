36a4ccfbfbb337b68a38ad788961a82a66f2b866
Wizzy-PC <wizzy83@gmail.com>
2023-01-21 12:08:36 +0100

    up

--- pokemon-image-classificator.py
+++ pokemon-image-classificator.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 
-generator = ImageDataGenerator(rescale=1/255, validation_split=0.2,
-                               width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-train_gen = generator.flow_from_directory('PokemonData/',
-                                          target_size=(220, 220), batch_size=32, class_mode='categorical',
-                                          subset='training')
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-test_gen = generator.flow_from_directory('PokemonData/',
-                                          target_size=(220, 220), batch_size=32, class_mode='categorical',
-                                          subset='validation')
+generator = ImageDataGenerator(rescale=1 / 255, validation_split=0.2, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+
+train_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=32, class_mode='categorical', subset='training')
+
+test_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=32,
+                                         class_mode='categorical', subset='validation')
 
 label_dict = train_gen.class_indices
 label_dict = {value: key for key, value in label_dict.items()}
@@ -26,8 +26,3 @@ random_idx = np.random.randint(0, 32, 1, int)[0]
 plt.imshow(sample_x[random_idx])
 plt.show()
 plt.title('true: %s \n predicted: %s' % (label_dict[sample_y[random_idx].argmax()], 'none'))
-
-
-
-
-
