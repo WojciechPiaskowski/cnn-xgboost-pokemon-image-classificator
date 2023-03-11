26226d1145fc9c0f6dba07a06d33225291994db1
Wizzy-PC <wizzy83@gmail.com>
2023-03-11 13:09:36 +0100

    experiments, analysis

--- pokemon_image_classificator.py
+++ pokemon_image_classificator.py
@@ -12,6 +12,7 @@ from keras.models import Model
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, log_loss, f1_score, accuracy_score
+from xgboost import XGBClassifier
 
 batch_size = 32
 
@@ -19,7 +20,9 @@ batch_size = 32
 generator = ImageDataGenerator(rescale=1 / 255, validation_split=0.2,
                                width_shift_range=0.12, height_shift_range=0.12, horizontal_flip=True,
                                brightness_range=[0.8, 1.0], zoom_range=[0.97, 1.0], rotation_range=20)
-train_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='training')
+
+train_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='training',
+                                          shuffle=True)
 test_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='validation',
                                          shuffle=False)
 
@@ -149,3 +152,63 @@ print(classification_report(test_gen.classes, yhat))
 
 mid_model = Model(cnn.input, cnn.get_layer('dense').output)
 mid_model.summary()
+
+# create a training dataset for xgboost out of imagedategenerator batches and CNN feature extraction
+
+for i in range(100):
+
+    if i == 0:
+        x_train_xgb, y_train_xgb = train_gen.next()
+        y_train_xgb = np.argmax(y_train_xgb, axis=1)
+        x_train_xgb = mid_model.predict(x_train_xgb)
+
+    else:
+        x_train_xgb2, y_train_xgb2 = train_gen.next()
+        y_train_xgb2 = np.argmax(y_train_xgb2, axis=1)
+        x_train_xgb2 = mid_model.predict(x_train_xgb2)
+
+        x_train_xgb = np.concatenate((x_train_xgb, x_train_xgb2), axis=0)
+        y_train_xgb = np.concatenate((y_train_xgb, y_train_xgb2), axis=0)
+
+
+test_gen.reset()
+for i in range(41):
+
+    if i == 0:
+        x_test_xgb, y_test_xgb = test_gen.next()
+        y_test_xgb = np.argmax(y_test_xgb, axis=1)
+        x_test_xgb = mid_model.predict(x_test_xgb)
+
+    else:
+        x_test_xgb2, y_test_xgb2 = test_gen.next()
+        y_test_xgb2 = np.argmax(y_test_xgb2, axis=1)
+        x_test_xgb2 = mid_model.predict(x_test_xgb2)
+
+        x_test_xgb = np.concatenate((x_test_xgb, x_test_xgb2), axis=0)
+        y_test_xgb = np.concatenate((y_test_xgb, y_test_xgb2), axis=0)
+
+
+
+
+xgb = XGBClassifier(objective='multi:softprob', num_class=150)
+
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+y_train_xgb = le.fit_transform(y_train_xgb)
+
+xgb.fit(x_train_xgb, y_train_xgb)
+
+
+
+
+
+yhat_xgb = xgb.predict(x_test_xgb)
+accuracy_score(y_test_xgb, yhat_xgb)
+
+
+
+import numpy as np
+
+np.random.randint(1, 3, 5)
