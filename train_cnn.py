e7ac619a9661677318ca6e1f468f9743c1a6e80a
Wizzy-PC <wizzy83@gmail.com>
2023-03-26 21:25:49 +0200

    dividing the CNN and xgboost training codes into seperate sripts

---
+++ train_cnn.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Conv2D, Flatten, Dense, Dropout, Input, LeakyReLU
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import joblib
+
+# set the batch size for the generator
+batch_size = 32
+
+# create a train and test data generator flowing from PokemonData subfolders
+generator = ImageDataGenerator(rescale=1 / 255, validation_split=0.2,
+                               width_shift_range=0.12, height_shift_range=0.12, horizontal_flip=True,
+                               brightness_range=[0.8, 1.0], zoom_range=[0.97, 1.0], rotation_range=20)
+
+train_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='training',
+                                          shuffle=True)
+test_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='validation',
+                                         shuffle=False)
+# label dictionary
+label_dict = train_gen.class_indices
+label_dict = {value: key for key, value in label_dict.items()}
+
+# create a CNN classifier
+i = Input(shape=(220, 220, 3))
+
+x = Conv2D(filters=32, kernel_size=(3, 3), strides=(2, 2), activation='relu')(i)
+# x = MaxPool2D()(x)
+# x = BatchNormalization()(x)
+
+x = Conv2D(filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
+# x = MaxPool2D()(x)
+# x = BatchNormalization()(x)
+
+x = Conv2D(filters=128, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
+# x = MaxPool2D()(x)
+# x = BatchNormalization()(x)
+
+x = Conv2D(filters=256, kernel_size=(3, 3), strides=(2, 2), activation='relu')(x)
+# x = MaxPool2D()(x)
+# x = BatchNormalization()(x)
+
+x = Flatten()(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.5)(x)
+x = Dense(len(train_gen.class_indices), activation='softmax')(x)  # 150 classes
+
+cnn = Model(i, x)
+
+# summary
+cnn.summary()
+
+# early stopping and model checkpoint callbacks
+es = EarlyStopping(monitor='val_loss', patience=5)
+checkpoint = ModelCheckpoint('models/cnn/', save_best_only=True)
+
+cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+r = cnn.fit_generator(generator=train_gen, epochs=100, steps_per_epoch=train_gen.samples // batch_size,
+                      shuffle=True, validation_data=test_gen, verbose=1, callbacks=[es, checkpoint])
+
+# model training plots
+
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend()
+plt.show()
+
+plt.plot(r.history['accuracy'], label='accuracy')
+plt.plot(r.history['val_accuracy'], label='val_accuracy')
+plt.legend()
+plt.show()
+
+joblib.dump(generator, 'other/image_gen.pkl')
+
---
+++ train_xgb.py
@@ -0,0 +1,77 @@
+import keras.models
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Model
+from xgboost import XGBClassifier
+import joblib
+
+# load in image data generators
+generator = joblib.load('other/image_gen.pkl')
+batch_size = 32
+
+train_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='training',
+                                          shuffle=True)
+test_gen = generator.flow_from_directory('PokemonData/', target_size=(220, 220), batch_size=batch_size, class_mode='categorical', subset='validation',
+                                         shuffle=False)
+
+# load the CNN model
+cnn = keras.models.load_model('models/cnn')
+
+# extract the CNN layers required for feature extraction before passing it through to XGBoostClassifier
+mid_model = Model(cnn.input, cnn.get_layer('dense').output)
+mid_model.summary()
+
+# I was unable to feed the image generator data directly to CNN->XGB, so I generate the training data 'manually'
+# create a training dataset for xgboost out of imagedategenerator batches and CNN feature extraction
+n = 1000
+for i in range(n):
+
+    if i == 0:
+        x_train_xgb, y_train_xgb = train_gen.next()
+        y_train_xgb = np.argmax(y_train_xgb, axis=1)
+        x_train_xgb = mid_model.predict(x_train_xgb)
+
+    else:
+        x_train_xgb2, y_train_xgb2 = train_gen.next()
+        y_train_xgb2 = np.argmax(y_train_xgb2, axis=1)
+        x_train_xgb2 = mid_model.predict(x_train_xgb2)
+
+        x_train_xgb = np.concatenate((x_train_xgb, x_train_xgb2), axis=0)
+        y_train_xgb = np.concatenate((y_train_xgb, y_train_xgb2), axis=0)
+
+    print(f'{i}/{n}')
+
+test_gen.reset()
+for i in range(41):
+
+    if i == 0:
+        x_test_xgb, y_test_xgb = test_gen.next()
+        y_test_xgb = np.argmax(y_test_xgb, axis=1)
+        x_test_xgb = mid_model.predict(x_test_xgb)
+
+    else:
+        x_test_xgb2, y_test_xgb2 = test_gen.next()
+        y_test_xgb2 = np.argmax(y_test_xgb2, axis=1)
+        x_test_xgb2 = mid_model.predict(x_test_xgb2)
+
+        x_test_xgb = np.concatenate((x_test_xgb, x_test_xgb2), axis=0)
+        y_test_xgb = np.concatenate((y_test_xgb, y_test_xgb2), axis=0)
+
+    print(f'{i}/41')
+
+# label encoder fixes an input issue with XGB classifier (expects Y to be sorted)
+le = LabelEncoder()
+y_train_xgb = le.fit_transform(y_train_xgb)
+
+# create the XGB model
+xgb = XGBClassifier(objective='multi:softprob', num_class=150, eval_metric=['mlogloss', 'merror'],
+                    learning_rate=0.2, max_depth=3, n_estimators=300,
+                    min_child_weight=50, reg_lambda=5,
+                    early_stopping_rounds=6)
+
+# fit the generated dataset to the XGB model
+xgb.fit(x_train_xgb, y_train_xgb,
+        eval_set=(x_test_xgb, y_test_xgb))
+
+# save the model object
+joblib.dump(xgb, 'models/xgb/xgb.pkl')
